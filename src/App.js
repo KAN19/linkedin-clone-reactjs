d4f58ecd82ef9a4c1b409fbeca8215a6ac5565fb
NguyenKiet <nguyenkiet0807@gmail.com>
2021-08-23 16:05:04 +0700

    header done.

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import Header from './Header';
 function App() {
   return (
     <div className="app">
-      <h1>Build Linkedin Clone</h1>
       {/** Header */}
       <Header/>
+
+
       {/** App body */}
         {/** Sidebar */}
         {/** Feed */}
--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,34 @@
 import React from 'react'
 import './Header.css'
+import SearchIcon from '@material-ui/icons/Search';
+import HeaderOptions from './HeaderOptions';
+import HomeIcon from '@material-ui/icons/Home';
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
+import WorkIcon from '@material-ui/icons/Work';
+import MessageIcon from '@material-ui/icons/Message';
+import NotificationsIcon from '@material-ui/icons/Notifications';
 
 function Header() {
   return (
     <div className="header">
-      <h1>This is Header</h1>
+      <div className="header__left">
+        <img src="https://image.flaticon.com/icons/png/512/174/174857.png" alt="logo" />
+        
+        <div className="header__search">
+          <SearchIcon />
+          <input type="text" />
+
+        </div>
+      </div>
+
+      <div className="header__right">
+        <HeaderOptions Icon={HomeIcon} title="Home"/>
+        <HeaderOptions Icon={SupervisorAccountIcon} title="My Network"/>
+        <HeaderOptions Icon={WorkIcon} title="Jobs"/>
+        <HeaderOptions Icon={MessageIcon} title="Messaging"/>
+        <HeaderOptions Icon={NotificationsIcon} title="Notifications"/>
+        <HeaderOptions avatar="https://image.flaticon.com/icons/png/512/4359/4359908.png" title="me"/>
+      </div>
     </div>
   )
 }

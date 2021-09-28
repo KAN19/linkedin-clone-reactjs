a92b9a8f747b8a4f3e23726c7fa9cfa0092a3826
Nguyen Kiet <nguyenkiet0807@gmail.com>
2021-09-28 11:23:36 +0700

    integrating firebase oke

--- src/Feed.js
+++ src/Feed.js
@@ -1,22 +1,79 @@
-import React from "react";
-import "./Feed.css";
+import CalendarViewDayIcon from "@material-ui/icons/CalendarViewDay";
 import CreateIcon from "@material-ui/icons/Create";
+import EventIcon from "@material-ui/icons/Event";
 import ImageIcon from "@material-ui/icons/Image";
 import SubscriptionsIcon from "@material-ui/icons/Subscriptions";
-import EventIcon from "@material-ui/icons/Event";
-import CalendarViewDayIcon from "@material-ui/icons/CalendarViewDay";
+import {
+  addDoc,
+  collection,
+  onSnapshot,
+  Timestamp,
+  orderBy,
+  query,
+} from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import "./Feed.css";
+import { db } from "./firebase";
 import InputOption from "./InputOption";
 import Post from "./Post";
 
 function Feed() {
+  const [posts, setPosts] = useState([]);
+  const [input, setInput] = useState("");
+
+  async function addPost() {
+    const data = {
+      name: "Nguyen Kiet",
+      description: "testing",
+      message: input,
+      photoUrl: "",
+      timestamp: Timestamp.now(),
+    };
+    try {
+      const docRef = await addDoc(collection(db, "posts"), data);
+
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  }
+
+  function updateData() {
+    const q = query(collection(db, "posts"), orderBy("timestamp", "desc"));
+    onSnapshot(q, (snapshot) => {
+      setPosts(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          data: doc.data(),
+        }))
+      );
+    });
+  }
+
+  useEffect(() => {
+    updateData();
+  }, []);
+
+  const sendPost = (e) => {
+    e.preventDefault();
+    addPost();
+    setInput("");
+  };
+
   return (
     <div className="feed">
       <div className="feed__inputContainer">
         <div className="feed__input">
           <CreateIcon />
           <form>
-            <input type="text" />
-            <button type="submit">Send</button>
+            <input
+              type="text"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
+            <button onClick={sendPost} type="submit">
+              Send
+            </button>
           </form>
         </div>
         <div className="feed__inputOptions">
@@ -35,11 +92,14 @@ function Feed() {
         </div>
       </div>
       {/* Post */}
-      <Post
-        name="nguyen kiet"
-        description="this is a test"
-        message="Hello ae"
-      />
+      {posts.map(({ id, data: { name, description, message } }) => (
+        <Post
+          key={id}
+          name={name}
+          description={description}
+          message={message}
+        />
+      ))}
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB0I0jbXLE_PXUhCdwDOaOADsSqaH0Ja2E",
+  authDomain: "linkedin-clone-d0ef9.firebaseapp.com",
+  projectId: "linkedin-clone-d0ef9",
+  storageBucket: "linkedin-clone-d0ef9.appspot.com",
+  messagingSenderId: "182922795593",
+  appId: "1:182922795593:web:3e5206bef8fb8504127d98",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+
+export { db };
